080792c3ddca69852a48eff867389b45966ee6c1
Chris Gregory <gregorybchris@gmail.com>
2024-08-10 11:58:13 -0700

    Add CORS

--- idtrace/app/app.py
+++ idtrace/app/app.py
@@ -1,6 +1,7 @@
 import logging
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from idtrace.app.service_routes import router as service_router
 from idtrace.app.vid_routes import router as vid_router
@@ -14,6 +15,14 @@ app = FastAPI()
 app = FastAPI(
     title="idtrace API",
     summary="Understand your users with personalized identifiers.",
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 app.include_router(service_router)
